666b459d9a3725c7b7fc8970ffcbef70ce1256ef
ahmedbentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-10-03 16:55:33 +0000

    Fix task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
     let task = true;
