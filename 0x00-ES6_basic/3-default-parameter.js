ef741ec9da8d612b5266ff7ed12109af50c052a1
ahmedbentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-10-05 07:30:45 +0000

    Fix lint error

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 
-export default (initialNumber, expansion1989 = 89, expansion2019 = 19) =>
-               (initialNumber + expansion1989 + expansion2019);
+export default (initialNumber, expansion1989 = 89, expansion2019 = 19) => {
+  return initialNumber + expansion1989 + expansion2019;
+}
