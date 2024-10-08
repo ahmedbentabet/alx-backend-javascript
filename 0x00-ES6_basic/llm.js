287d62737ab8abc0118a8af5fa4bb95d56784379
ahmedbentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-10-08 14:55:12 +0000

    Add all files

--- 0x00-ES6_basic/llm.js
+++ 0x00-ES6_basic/llm.js
@@ -1,5 +1,22 @@
-#!/usr/bin/node
 
-import { taskFirst, taskNext } from './0-constants.js';
 
-console.log(`${taskFirst()} ${taskNext()}`);
+
+
+
+
+
+
+
+
+// Spread With Objects => Merge Objects
+
+let objOne = {
+  a: 1,
+  b: 2,
+};
+let objTwo = {
+  c: 3,
+  d: 4,
+};
+
+console.log({ ...objOne, ...objTwo, e: 5 });
