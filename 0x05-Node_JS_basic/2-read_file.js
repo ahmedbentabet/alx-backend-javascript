604679e9abe873f2ea7e228b88c801331422a871
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-12-16 07:34:15 +0000

    Add tasks 1 & 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,37 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(filePath) {
+  if (!fs.existsSync(filePath)) {
+    throw new Error('Cannot load the database');
+  }
+
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const rows = fileContent.split('\n');
+  const studentByField = {};
+  let totalStudents = 0;
+
+  for (const row of rows) {
+    if (row.trim() !== '') {
+      const columns = row.split(',');
+      const studentField = columns[3];
+      const studentName = columns[0];
+
+      if (totalStudents > 0) {
+        if (!Object.hasOwnProperty.call(studentByField, studentField)) {
+          studentByField[studentField] = [];
+        }
+        studentByField[studentField].push(studentName);
+      }
+      totalStudents += 1;
+    }
+  }
+
+  console.log(`Number of students: ${totalStudents - 1}`);
+  for (const field in studentByField) {
+    if (Object.hasOwnProperty.call(studentByField, field)) {
+      console.log(`Number of students in ${field}: ${studentByField[field].length}. List: ${studentByField[field].join(', ')}`);
+    }
+  }
+}
+
+module.exports = countStudents;
