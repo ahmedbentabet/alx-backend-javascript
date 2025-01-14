6f8d4ccd0a171445b25f8c694e480c69deba82c2
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2025-01-14 15:01:18 +0000

    Add task 1

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,19 +3,19 @@ const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function () {
   it('should add two numbers', function () {
-    assert.strictEqual(calculateNumber('SUM', 3.7, 2.5), 7);
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
   });
 
   it('should subtract two numbers', function () {
-    assert.strictEqual(calculateNumber('SUBTRACT', 3.7, 2.5), 1);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   });
 
   it('should divide two numbers', function () {
-    assert.strictEqual(calculateNumber('DIVIDE', 8.4, 2.5), 3);
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
   });
 
   it('should return Error when dividing by zero', function () {
-    assert.strictEqual(calculateNumber('DIVIDE', 3.7, 0), 'Error');
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
   });
 
 });
