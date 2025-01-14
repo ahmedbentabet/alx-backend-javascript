ff8962860bede77f0e5a0e852fe962b66564d293
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2025-01-14 15:18:31 +0000

    Add task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,17 +3,10 @@ const sendPaymentRequestToApi = require('./3-payment.js');
 const Utils = require('./utils.js');
 
 describe('sendPaymentRequestToApi', function () {
-  it('should use Utils.calculateNumber with SUM', function () {
+  it('should use Utils.calculateNumber', function () {
     const spy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(150, 30);
-    sinon.assert.calledWith(spy, 'SUM', 150, 30);
-    spy.restore();
-  });
-
-  it('should use Utils.calculateNumber with different values', function () {
-    const spy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(200, 10);
-    sinon.assert.calledWith(spy, 'SUM', 200, 10);
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWith(spy, 'SUM', 100, 20);
     spy.restore();
   });
 });
